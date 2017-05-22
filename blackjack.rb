c1d1b47c6836c13ab9abbe27e34a2e8ccfb60736
Tate Jeppesen <jeppesenlelouch@gmail.com>
2017-05-22 11:26:45 -0600

    initial black jack

---
+++ blackjack.rb
@@ -0,0 +1,8 @@
+require_relative 'Casino'
+
+class BlackJack
+  def initialize
+    puts "Welcome to black jack!!!!! Where the jack is black and Black Jack Black plays!!!"
+    binding.pry
+  end
+end
