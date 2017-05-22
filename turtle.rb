aacdea8d984f120adde91fc919e3ec6650ec031b
Addy King <ashepherdking@gmail.com>
2017-05-22 14:37:33 -0600

    turtle update

--- turtle.rb
+++ turtle.rb
@@ -1,9 +1,29 @@
 require_relative 'player'
 
 class Turtle
-
+  attr_accessor :player
   def initialize(player)
-    puts "Welcome to Turtle Racing!"
+
+    @turtle_name = ["Timmy", "Boris", "Gordy", "Dave", "Sammy"]
+    @player = player
+
+    puts "<-----------------Welcome to Turtle Racing!------------------>"
+    puts " "
+    puts " "
+    puts " "
+#main_menu
+    puts @turtle_name
+    turtle_name = gets.strip.to_s
+    if @turtle_name.include?(turtle_name)
+      bets
+    else
+      puts "Invalid turtle name!"
+      Turtle.new(player)
+    end
+  end
+
+  def bets
+    puts "You have $#{@player.wallet.amount}"
 
   end
 end
