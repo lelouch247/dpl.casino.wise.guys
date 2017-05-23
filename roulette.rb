33a27e25450729451cf5775d4f1d66d4daf920e0
Alan Vincent <alan.d.vincent@gmail.com>
2017-05-23 11:13:54 -0600

    added color betting and staright betting to roulette

--- roulette.rb
+++ roulette.rb
@@ -3,17 +3,13 @@ require_relative 'player'
 class Roulette
   attr_accessor :player
   def initialize(player)
+    @player = player
+    @bet_amount = 0
+    @all_colors = ["red", "black"]
+    @on_color = nil
+    @on_number = nil
     roulette_menu
   end
-
-  @bet_amount = 0
-  @all_numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-                11, 12, 13, 14, 15, 16, 17, 18, 19,
-                20, 21, 22, 23, 24, 25, 26, 27, 28,
-                29, 30, 31, 32, 33, 34, 35, 36]
-
-  @all_colors = ["red", "green"]
-
 
   def place_bet
     @bet_amount = 0
@@ -29,6 +25,15 @@ class Roulette
 
   def straight_up
     place_bet
+    puts "What number would you like to place your bet on? (1-36)"
+    my_number = gets.strip.to_i
+    puts "Spinning!!!!!"
+    @on_number = rand(37)
+    puts "The ball lands on #{@on_number}!"
+    my_number == @on_number ? (puts "Winner!") : (puts "Better luck next spin!")
+    my_number == @on_number ? (@player.wallet.amount += @bet_amount * 35) : (@player.wallet.amount -= @bet_amount)
+
+    roulette_menu
   end
 
   def column
@@ -41,6 +46,14 @@ class Roulette
 
   def color_bet
     place_bet
+    @on_color = nil
+    puts "Would you like to bet on red or black?"
+    color_choice = gets.strip
+    @on_color = @all_colors.sample
+    puts "and the ball lands on...#{@on_color}"
+    color_choice == @on_color ? (@player.wallet.amount += @bet_amount) : (@player.wallet.amount -= @bet_amount)
+    roulette_menu
+
   end
 
   def main_menu_choice
@@ -72,5 +85,6 @@ class Roulette
     puts "4. Straight up (pick only one number) - pays 35:1"
     puts "5. Street (bet on 3 numbers) - pays 11:1"
     puts "6. Back to main casino"
+    main_menu_choice
   end
 end
